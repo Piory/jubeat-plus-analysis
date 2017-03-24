6bcce12c7a48066397812cda21698ea970d4a02b
yoshiki_umeda <aiueo8102@gmail.com>
2017-03-24 13:46:47 +0900

    update google oauth2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,4 +271,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  config.omniauth :google_oauth2,
+                  Settings.omniauth.google_oauth2.client_id,
+                  Settings.omniauth.google_oauth2.client_secret
 end
